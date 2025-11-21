5938c3a873af84f1c554d07fbd17812e120deb43
MCPsDEPE <38997081+MCPsDE@users.noreply.github.com>
2025-11-21 20:58:33 +0800

    Implement mandatory callback functions for notes
    Added required callback functions for note handling.

--- Skin/Demo/LoadMSModule.lua
+++ Skin/Demo/LoadMSModule.lua
@@ -1,3 +1,6 @@
 function Init()
     Module:Find("tep").Text = load(Game:ReadBytes("tep.ms"):ToArray())()
 end
+-- 下面两个回调函数是必须存在的
+function OnDrawNote(info) end
+function OnNoteMove(info, mod, percent) end
