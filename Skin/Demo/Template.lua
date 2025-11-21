e8f464ac68b9a7ae32828e93b9c88d12ba8f0ac6
MCPsDEPE <38997081+MCPsDE@users.noreply.github.com>
2025-11-21 19:08:47 +0800

    Add Template.lua with initial function definitions

---
+++ Skin/Demo/Template.lua
@@ -0,0 +1,12 @@
+function CanPlay() end
+function Init() end
+function OnRetry() end
+function OnProcessNote() end
+function OnDrawNote(info) end
+function OnNoteMove(info, mod, percent) end
+function OnFinish() end
+function Update() end
+function OnHit() end
+function OnInput() end
+function OnKey() end
+function ReportCustomInfo() end
