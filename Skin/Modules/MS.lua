cccd9bb2b713db0308aae154e617e0fe865fc02a
mcpsde <3011991426@qq.com>
2025-11-22 16:21:56 +0800

    No MS.lua

--- Skin/Modules/MS.lua
+++
@@ -1,9 +0,0 @@
-local safe_load = load
-load=nil
-_G.load=nil
-string.dump=nil
-_G.string.dump=nil
-MS={}
-function MS:Load(MSModuleName)
-    return safe_load(Game:ReadBytes(MSModuleName):ToArray())
-end
